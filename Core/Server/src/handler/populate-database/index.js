e8c5fba80f0649ad8b95d47934ddc2f6f3f64745
Femi <femiadesina@live.co.uk>
2019-12-19 10:37:34 +0000

    Added stuff

--- Core/Server/src/handler/populate-database/index.js
+++ Core/Server/src/handler/populate-database/index.js
@@ -1,16 +1,13 @@
 import 'core-js'
 import expressServer from 'toolbox/express/initiate-express'
 import serverlessExpress from 'toolbox/express/serverless-express'
-import * as pokemonMiddleware from 'middleware/pokemon'
+import * as pokemonDataMiddleware from 'middleware/pokemon'
 import * as ROUTES from 'routes'
 
 const app = expressServer() // Gets the koa object instatiated from the toolbox
 
-// Pokemon Routes
-app.get(ROUTES.GET_POKEMON, pokemonMiddleware.getPokemon)
-app.get(ROUTES.GET_ALL_POKEMON, pokemonMiddleware.getAllPokemon)
-app.post(ROUTES.ADD_NEW_POKEMON, pokemonMiddleware.addNewPokemon)
-app.post(ROUTES.UPDATE_POKEMON, pokemonMiddleware.updatePokemon)
-app.post(ROUTES.DELETE_POKEMON, pokemonMiddleware.deletePokemon)
+// Private Routes
+app.post(ROUTES.POPULATE_POKEMON_TABLE, pokemonDataMiddleware.populatePokemonTable)
+app.post(ROUTES.POPULATE_POKEMON_TYPE_TABLE, pokemonDataMiddleware.populatePokemonTypeTable)
 
 export const handler = (event, context) => serverlessExpress(app, event, context)
